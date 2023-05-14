7a8c45b549f9cfb073c500577dc2042503e16ac1
bel <126126442+k2tsune@users.noreply.github.com>
2023-05-13 23:55:39 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,21 @@
+
+
+const botaoAlterarTema = document.getElementById("bota-alterar-tema");
+const body = document.querySelector("body");
+const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao")
+
+
+botaoAlterarTema.addEventListener("click", () => {
+
+
+    const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
+
+    body.classList.toggle("modo-escuro");
+    if (modoEscuroEstaAtivo) {
+        imagemBotaoTrocaDeTema.setAttribute("src", "./src/images/sun.png");
+    } else {
+
+
+        imagemBotaoTrocaDeTema.setAttribute("src", "./src/images/moon2.png");
+    }
+});
